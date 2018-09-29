b532cbf6b090d0c9a3d30477f388aa009c03e9d6
蒋银芝 <jiangyinzhi@ssgm.net>
2018-09-29 10:53:42 +0800

    添加关于three的依赖

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import Login from '@/views/login'
 Vue.use(Router)
 
+export const RouterMap = [
+  { path: '/login', name: 'Login',  component: Login}
+]
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  mode:'history',
+  routes: RouterMap
 })
---
+++ src/views/login/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="main-contain">
+
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+
+    }
+  },
+  methods:{
+  },
+  mounted:{
+
+  }
+}
+</script>
+
+<style scoped>
+.main-contain{
+  width: 100vw;
+  height: 100vh;
+}
+</style>
